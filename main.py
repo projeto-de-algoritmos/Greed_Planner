2c203fd3aae54ca32e6290ff2154484439709cad
FabianaRibas <fabianalv.ribas@gmail.com>
2021-09-05 13:10:02 -0300

    adding route to get all tasks

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from json_db import jsonDB
 
 
 app = FastAPI()
+
+db = jsonDB()
+
+
+class Task(BaseModel):
+    task_day: str
+    start_time: str
+    end_time: str
+    name: str
+    description: str
 
 
 @app.get("/")
 def root():
     return {"message": "sucess"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    data = db.get_all(table_name="tasks")
+
+    return data
